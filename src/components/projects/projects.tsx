8781ad122419ff7716b9c1e9563cdc4988e16d1a
Sean Dahan <aui.svi@gmail.com>
2025-02-08 10:10:14 +0200

    Progress

--- src/components/projects/projects.tsx
+++ src/components/projects/projects.tsx
@@ -1,14 +1,31 @@
+import SingleProject from "./singleproject";
+
+const porjects = [
+  {
+    name: "Das Tracks",
+    description:
+      "Connects riders over the world with track owners lets them see closest track to them, see opening hours and more.",
+    githubLink: "https://github.com/frostn1",
+    tags: ["express", "react", "typescript"],
+    desktopImage: "dastrack-desktop.png",
+    mobileImage: "",
+  },
+];
 
 const Projects = () => {
   return (
     <div id={"projects"}>
-      <h1 className="text-3xl">Projects</h1>
-      <p className="text-wrap w-[300px] text-(--subtext)">
-        Lorem ipsum dolor sit, amet consectetur adipisicing elit. Ratione
-        corrupti iste a dolor. Sit suscipit alias quae veniam eligendi,
-        molestiae eveniet mollitia iusto tempora ex culpa minus fuga adipisci
-        consequatur?
-      </p>
+      {/* <h1 className="text-3xl">Projects</h1> */}
+      {porjects.map((_project) => (
+        <SingleProject
+          name={_project.name}
+          description={_project.description}
+          githubLink={_project.githubLink}
+          tags={_project.tags}
+          desktopImage={_project.desktopImage}
+          mobileImage={_project.mobileImage}
+        />
+      ))}
     </div>
   );
 };
---
+++ src/components/projects/singleproject.tsx
@@ -0,0 +1,56 @@
+import React from "react";
+import { Button } from "../ui/button";
+import { Safari } from "../magicui/safari";
+import Iphone15Pro from "../magicui/iphone-15-pro";
+
+interface SingleProjectProps {
+  name: string;
+  description: string;
+  githubLink: string;
+  tags: string[];
+  desktopImage: string;
+  mobileImage: string;
+}
+
+const SingleProject: React.FC<SingleProjectProps> = ({
+  name,
+  description,
+  githubLink,
+  tags,
+  desktopImage,
+  mobileImage,
+}) => {
+  return (
+    <div className="grid grid-cols-2">
+      <div className="flex flex-col gap-5 ">
+        <div className="flex flex-row items-baseline gap-3">
+          <span className="text-2xl bold">{name}</span>
+          <Button variant="link" className="text-(--subtext) p-0 h-fit">
+            <a href={githubLink} rel="noreferrer" target="_blank">
+              GitHub
+            </a>
+          </Button>
+        </div>
+        <div className="text-wrap text-(--subtext) w-md">{description}</div>
+        <div className="flex flex-row gap-3">
+          {tags.map((tag) => (
+            <div className={"border rounded-3xl text-sm p-1 pl-3 pr-3 "}>
+              {tag}
+            </div>
+          ))}
+        </div>
+      </div>
+      <div>
+        <Safari
+          mode="simple"
+          url="das-track.web.app"
+          imageSrc={desktopImage}
+          className="size-full"
+        />
+        {/* <Iphone15Pro className="size-full" /> */}
+      </div>
+    </div>
+  );
+};
+
+export default SingleProject;
